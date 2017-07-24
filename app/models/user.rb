be5b0d1350ed78796ad69dbd1dd558ded8624ddb
David Astor <toodimes@gmail.com>
2017-07-24 18:59:19 -0400

    working facebook omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_one :trainer_profile
   has_one :user_profile
@@ -17,13 +18,32 @@ class User < ApplicationRecord
   has_many :users, foreign_key: "trainer_id"
 
   after_create :create_profile
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # user.user_profile.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  # def self.new_with_session(params, session)
+  #   super.tap do |user|
+  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+  #       user.email = data["email"] if user.email.blank?
+  #     end
+  #   end
+  # end
 
   def create_profile
     if trainer_application
       TrainerProfile.create(name: "Name", gender: 1, bio: "bio", user_id: self.id)
     else
       UserProfile.create(name: "Name", gender: 1, user_id: self.id)
-      # self.update(trainer_id: 7)
+      self.update(trainer_id: 33)
     end
     Avatar.create(url: "https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg", user_id: self.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724143020) do
+ActiveRecord::Schema.define(version: 20170724150316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,8 @@ ActiveRecord::Schema.define(version: 20170724143020) do
     t.boolean  "trainer_application",    default: false
     t.integer  "trainer_id"
     t.boolean  "admin",                  default: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
